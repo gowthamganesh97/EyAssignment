20df29a92fbd831b267f831be77bba1d6429880a
GOWTHAMGANESH <100587813+gowthamganesh97@users.noreply.github.com>
2024-06-07 14:56:00 +0530

    AuthorService.java

--- Day9Assignment/Q3/AuthorService.java
+++ Day9Assignment/Q3/AuthorService.java
@@ -6,7 +6,6 @@ import java.util.Set;
 import com.ey.day9.Q1.Author;
 
 public interface AuthorService{
-	//Get the unique surnames in uppercase
 	public abstract Set<String> getUniqueSurnames(List<Author> authorList);
 	public abstract List<Author> getAuthorsByCity(List<Author> authorList,String city);
 	public abstract double femaleAverageAge(List<Author> authorList);
