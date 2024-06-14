05424b801c0b701ed4c7bf7a09320b3783cbaa3a
GOWTHAMGANESH <100587813+gowthamganesh97@users.noreply.github.com>
2024-06-14 09:56:31 +0530

    Product.java

--- JDBC/day13/Product.java
+++ JDBC/day13/Product.java
@@ -1,5 +1,21 @@
 package com.ey.day13;
 
-public class Product {
+public class Product
+{
+		Integer id;
+		String name;
+		String email;
+		public Product(Integer id,String name,String email) {
+			super();
+			this.id=id;
+			this.name=name;
+			this.email=email;
+		}
+		@Override
+		public String toString() 
+		{
+			return "Product [id=" + id + ", name=" + name + ", email=" + email + "]";
+		}
+	
 
 }
